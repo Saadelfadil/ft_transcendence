4b7c196a9f0c665dabdace15d1a6cc44fdf1ce43
sel-fadi <sel-fadi@student.1337.ma>
2022-03-22 12:43:32 +0100

    Start with NestJS

--- auth-api/src/auth/auth.service.ts
+++ auth-api/src/auth/auth.service.ts
@@ -1,7 +1,16 @@
 import { Injectable } from '@nestjs/common';
-import { UsersService } from 'src/users/users.service';
+import { User, UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
     constructor(private usersService: UsersService) {}
+    async validateUser(username: string, password: string) : Promise<any> {
+        const user = await this.usersService.findOne(username);
+
+        if (user && user.password === password)
+        {
+            return `The user is ${user.name}`;
+        }
+        return null;
+    };
 }
--- auth-api/src/users/users.module.ts
+++ auth-api/src/users/users.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from 'src/auth/local.strategy';
 import { UsersService } from './users.service';
 
 @Module({
-  providers: [UsersService],
-  exports: [UsersService]
+  providers: [UsersService, PassportModule],
+  exports: [UsersService, LocalStrategy]
 })
 export class UsersModule {}
